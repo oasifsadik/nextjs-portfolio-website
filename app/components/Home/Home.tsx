8ce19cee6e4c112e5070f7e8506762f46c2c6f53
oasifsadik <oasifsadik99@gmail.com>
2025-12-19 22:08:45 +0600

    home page design

---
+++ app/components/Home/Home.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="overflow-hidden">
+      <h1 className="text-4xl font-bold text-white">Welcome to My Portfolio</h1>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans} antialiased bg-[#0d0d1a]` }
+        className={`${geistSans} antialiased bg-[#0d0d1a] ` }
       >
         {children}
       </body>
